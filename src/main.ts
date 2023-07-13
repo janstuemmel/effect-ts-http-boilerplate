dee53ba4d4757227f2e3d9d50a84cbc7be3a99d4
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-07-13 20:54:54 +0200

    cleanup main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import http from 'http';
-import findMyWay, { Res, Req } from 'find-my-way';
+import findMyWay, {  } from 'find-my-way';
 
-import * as Effect from '@effect/io/Effect';
 import * as Layer from '@effect/io/Layer';
 import { pipe } from '@effect/data/Function';
 
 import {getTodoHandler} from './todo/handler.js';
 import { Config } from './common/config/config.js';
 import { TodoClientLayer } from './common/client/todoClient.js';
-import { HttpError } from './common/http/errors.js';
 
 import stageConfig from '../config/stage.js';
-import { PathParams, AppRequest, AppResponse, createAppResponse } from './common/http/http.js';
 import { wrapRequest } from './common/http/wrapRequest.js';
 
 // di
